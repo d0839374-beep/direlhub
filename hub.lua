cc7bd1545320edd72f5390dbe7fada2e49f110f2
d0839374-beep <d0839374@gmail.com>
2025-08-24 20:42:53 +0700

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,31 +1,19 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
 
-local Window = Library.CreateLib("DIRELHUB", "RJTheme3")
+local Main = library:CreateWindow("Main","Crimson")
 
-local Tab = Window:NewTab("Player")
+local tab = Main:CreateTab("Player")
 
-local Section = Tab:NewSection("Speed")
-
-Section:NewSlider("Speed", "speed 0-500", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+tab:CreateSlider("Jump power",1,500,function(s)
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
 end)
 
-Section:NewTextBox("Speed", "speed", function(e)
-  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = e
+tab:CreateSlider("Speed",1,500,function(s)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-local Section = Tab:NewSection("Jump power")
-
-Section:NewSlider("JumpPower", "высота прыжка", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
+tab:CreateButton("fly",function(s)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
 end)
 
-Section:NewTextBox("JumpPower", "высота прыжка", function(s)
-  game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
-end)
-
-local Section = Tab:NewSection("fly")
-
-Section:NewButton("fly", "Полет", function(s)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-end)
+local tab2 = Main:CreateTab("Misc")
